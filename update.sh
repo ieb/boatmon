a5e09140a42c3888b035f3f3c3dd20e106d6aa07
Ian Boston <ieb@tfd.co.uk>
2025-10-11 10:26:09 +0100

    Added Update

--- update.sh
+++ update.sh
@@ -6,6 +6,7 @@ afterSha1=$(git rev-parse HEAD)
 if [ "a${berforeSha1}" != "a${afterSha1}" ]
 then
 	echo Boatmon OTA New Version Detected
+	git log | head -3
 	uv sync
 	killall -e /home/ieb/boatmon/.venv/bin/python3
 fi
